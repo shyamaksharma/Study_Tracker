56e9cf99d2bdfd5330f54037700646bddda2bf25
shyamaksharma <shyamak.25bai10739@vitbhopal.ac.in>
2025-11-24 22:48:06 +0530

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,11 +1,15 @@
 def day():
     sbj = input("Which Subject did you study today? ")
     minu = int(input(f"How many minutes did you study {sbj} today? "))
+    
     subjects = []
     mins = []
+    
     subjects.append(sbj)
     mins.append(minu)
+    
     a = input("Did you study anything else? (Y/N) ")
+    
     if a.lower() in ["yes","y","yep"]:
         b = int(input("How many other subjects did you study? "))
         for i in range (0,b):
@@ -13,4 +17,5 @@ def day():
             hrs2 = int(input(f"How long did you study {sbj2} for? "))
             subjects.append(sbj2)
             mins.append(hrs2)
+            
     return subjects, mins
