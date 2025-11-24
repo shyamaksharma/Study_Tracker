813261a91a7ba405f33f31eb7ade0bbd8f3fb491
shyamaksharma <shyamak.25bai10739@vitbhopal.ac.in>
2025-11-24 22:38:26 +0530

    Delete output.py

--- output.py
+++
@@ -1,5 +0,0 @@
-def output(maxsub1,minsub1,day1,mins,subjects):
-    if len(subjects) == 1:
-        print(f"you studied {maxsub1} for {day1} minutes today")
-    else:
-        print(f"you studied {maxsub1} the most and {minsub1} the least and you studied for a total {day1} minutes today")
