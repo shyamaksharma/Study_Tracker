84b5a4d8d7a230775017e45b2d7974c6a87c24f0
shyamaksharma <shyamak.25bai10739@vitbhopal.ac.in>
2025-11-24 22:38:19 +0530

    Delete processing.py

--- processing.py
+++
@@ -1,9 +0,0 @@
-def daily_calc(subjects, mins):
-    for i in range (len(mins)):
-        print(subjects[i],":", mins[i])
-    day1 = sum(mins)
-    index1 = mins.index(max(mins))
-    maxsub1 = subjects[index1]
-    index2 = mins.index(min(mins))
-    minsub1 = subjects[index2]
-    return maxsub1, minsub1, day1   
